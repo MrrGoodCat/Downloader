432776870845d4dfff646f4b76dc708957a8a8d0
MrrGoodCat <mrrgoodcat@gmail.com>
2016-10-12 01:49:58 +0300

    sdvs

---
+++ Downloader/FileDownloader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Downloader
+{
+    public class FileDownloader
+    {
+        int start;
+        int count;
+        string pathTemp;
+        string url;
+
+        public FileDownloader(string url, int start, int count)
+        {
+            this.url = url;
+            this.start = start;
+            this.count = count;
+            pathTemp = Path.GetTempFileName();
+        }
+        public void DoDownload()
+        {
+
+        }
+    }
+}
